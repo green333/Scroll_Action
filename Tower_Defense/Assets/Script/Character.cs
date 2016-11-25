4570c1a48af5b6f5b186ad862118b558050246b6
green333 <jqgjurhjmbjgjadmtj54@gmail.com>
2016-11-25 16:35:46 +0900

    新しいゲーム作成
    Brave_Projectから派生させてタワーディフェンスの作成

---
+++ Tower_Defense/Assets/Script/Character.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[System.Serializable]
+class Status
+{
+    public int hp;        //体力
+    public int attack;    //攻撃
+    public int defense;   //防御
+    public float spd;     //速度
+}
+
+
+
+public class Character : MonoBehaviour {
+
+    //-----------------------
+    //  プレイヤーの状態
+    //-----------------------
+    protected enum P_State
+    {
+        MOVE = 0,                 //操作する
+        ATTACK,                   //攻撃する
+    }
+
+    [SerializeField]
+    private P_State p_state;
+
+    [SerializeField]
+    Status info;      //ステータス情報
+
+    [SerializeField]
+    private new Rigidbody rigidbody;     //Rigidbody2D
+
+    [SerializeField]
+    protected Animator animator;          //アニメーション
+
+
+
+
+    //------------------------------------------
+
+    //  初期化
+
+    //------------------------------------------
+    void Start()
+    {
+
+    }
+
+    //------------------------------------------
+
+    //  更新
+
+    //------------------------------------------
+    void Update()
+    {
+        switch (p_state)
+        {
+            case P_State.MOVE:
+                Move();
+                break;
+
+            case P_State.ATTACK:
+                break;
+        }
+
+    }
+
+
+    //------------------------------------------
+
+    //  物理挙動がある時
+
+    //------------------------------------------
+    void FixedUpdate()
+    {
+
+    }
+
+
+    //------------------------------------------
+
+    //  移動
+
+    //------------------------------------------
+    public void Move()
+    {
+        rigidbody.velocity = new Vector2(info.spd, rigidbody.velocity.y);
+    }
+
+
+
+    //------------------------------------------
+
+    //  範囲内に入れば動かない
+
+    //------------------------------------------
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            p_state = P_State.ATTACK;
+        }
+    }
+
+
+    //------------------------------------------
+
+    //  範囲外に行けば動く
+
+    //------------------------------------------
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            p_state = P_State.MOVE;
+        }
+    }
+
+}
---
+++ Tower_Defense/Assets/Script/Home.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Home : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject[] players;
+
+    //---------------------------------------
+
+    //  初期化
+
+    //---------------------------------------
+    void Start () {
+	
+	}
+
+    //---------------------------------------
+
+    //  更新
+
+    //---------------------------------------
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Create_Character();
+        }
+	}
+
+
+
+    void Create_Character()
+    {
+        Instantiate(players[0], transform.position, Quaternion.identity);
+    }
+}
